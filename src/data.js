d402f40403cb81e81d6f6855be4904d6b674dac5
Marcelo Rodrigues Rosa Paschoal <marcrodprv@gmail.com>
2026-06-03 19:14:21 -0300

    Fiz as bruxas

--- src/data.js
+++ src/data.js
@@ -51,7 +51,7 @@ const culturas = [       //Raça
                 ao se deparar com diversas outras culturas diferentes, acaba por rejeitá-las como sociedade. 
                 A intolerância da humanidade levou as outras culturas a se isolarem dos humanos como medida 
                 protetiva contra a possibilidade de instinção, até hoje existem humanos caçadores de vampiros, 
-                bruxas, lobisomens, ..., enfim, tais caçadores possuem conhecimentos de artefatos, rituais e 
+                bruxas, lobisomens, etc, enfim, tais caçadores possuem conhecimentos de artefatos, rituais e 
                 o mais perigoso: as fraquezas letais das demais culturas.
             </p>
         `
@@ -71,6 +71,23 @@ const culturas = [       //Raça
                 expecífica por dedicarem seu ilimitado tempo nela, pode ser das artes místicas dos rituais à 
                 algum instrumento musical.
             </p>
+        `
+    },
+    {
+        id: 2,
+        nome: 'Bruxas',
+        aparencia_atipica: 'Ninguém conhece a verdadeira aparência de uma bruxa, todas optaram por tomar uma forma humanizada, porém há sempre uma pequena irregularidade que passa despercebido',
+        pontosFortes: ['Manipula energia arcana de forma natural'],
+        pontosFracos: ['Qualquer ritual feito por uma bruxa é potente de mais para ser controlado, gerando resultados catastróficos', 'O ritual Chama Sagrada queima a alma de uma bruxa destruindo-a para sempre'],
+        historia: `
+            <p class="paragrafo">
+                    As bruxas, e em casos muito raros bruxos, possuem duas almas, uma referente a consciência e outra que 
+                é passada de geração em geração, a qual carrega aquilo que as torna bruxas. Existem muito mais bruxas do que 
+                bruxos, sensato considerando que a existência de um bruxo significa o fim daquela linhagem, pois apenas uma mãe 
+                pode passar a segunda alma dela adiante. De maneira geral, manipulam energia arcana como ninguém, podendo criar 
+                magias,poções, maldições, encantamentos, artefatos, invocações, etc. Costumam preferir a propria companhia do 
+                que fazer relações sociais, e ter uma personalidade egocentrica e arrogante. 
+            </P>
         `
     }
 ];
